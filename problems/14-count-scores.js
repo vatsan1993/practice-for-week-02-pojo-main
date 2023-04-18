08bb94c251ca1ef0c31e9a704a0fa8a7797b387b
Srivatsan <vatsan1993@gmail.com>
2023-04-18 16:17:30 +0530

    finished all programs

--- problems/14-count-scores.js
+++ problems/14-count-scores.js
@@ -28,7 +28,16 @@ console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
 ***********************************************************************/
 
 function countScores(people) {
-  // Your code here
+  let scores = {};
+  people.forEach(person => {
+    let {name, score} = person;
+    if(scores[name] === undefined){
+      scores[name] = score;
+    }else{
+      scores[name] += score;
+    }
+  });
+  return scores;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
