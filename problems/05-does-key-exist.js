a4fea653f4096ecf8945b59c978a8f4cdf4437d2
Srivatsan <vatsan1993@gmail.com>
2023-04-18 14:33:57 +0530

    finished 5th problem

--- problems/05-does-key-exist.js
+++ problems/05-does-key-exist.js
@@ -12,6 +12,7 @@ doesKeyExist(obj1, 'name'); // => false
 
 function doesKeyExist(obj, key) {
   // Your code here
+  return obj[key] !== undefined
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
